4ec757d00f572e403de1700e553a8c79944ea06b
gauravshonik <gauravshonik01@gmail.com>
2023-05-09 16:00:13 +0530

    file added

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector(".fa-xmark");
+const navaction = document.querySelector(".navaction");
+const sidenavigation = document.querySelector(".sidenavigation");
+
+bars.addEventListener("click", () => {
+  sidenavigation.classList.toggle("showdisplay");
+})
+
+xmark.addEventListener("click", () => {
+    sidenavigation.classList.toggle("showdisplay");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
